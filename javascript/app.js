fdac6525745af3014103316584721c28a6c92b30
Luke Rickard <luke.x.rickard@gmail.com>
2016-06-07 16:58:15 +0100

    initial commit for moviesDB site

---
+++ javascript/app.js
@@ -0,0 +1,27 @@
+// defining the app module of the project
+angular.module('moviesDBApp', ['ngRoute','movieDBControllers','movieDBDirectives','movieDBServices'])
+.constant("myMovieConfig", {
+        "moviesEndpoint" : "https://api.themoviedb.org/3/movie",
+        "apiKey": "778083a2f93090ad5fc8e4f1aeac745f"
+    })
+.config(function($routeProvider) {
+		 
+		$routeProvider
+		  .when('/popular', {
+		  	templateUrl: 'templates/movies.html',
+		  	controller: 'MovieListController'
+		  })
+		  .when('/upcoming', {
+		  	templateUrl: 'templates/movies.html',
+			controller: 'MovieUpcomingController'
+		  })
+		  .when('/topRated', {
+		  	templateUrl: 'templates/movies.html',
+			controller: 'MovieTopRatedController'
+		  })
+		  .when("/nowPlaying", {
+			templateUrl: "templates/movies.html",
+			controller: "MovieNowPlayingController"
+		})
+		  .otherwise({redirectTo: '/popular'}); 
+	});
